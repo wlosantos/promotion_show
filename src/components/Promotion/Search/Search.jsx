89d5bc596b569ef5878f468465e67df824f58556
Wendel Lopes <wendelopes@msn.com>
2021-06-05 07:08:28 -0300

    Adding search component

--- src/components/Promotion/Search/Search.jsx
+++ src/components/Promotion/Search/Search.jsx
@@ -1,18 +1,38 @@
+import './Search.css'
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 import PromotionCard from '../Card/Card'
 
 const PromotionSearch = () => {
 
   const [promotions, setPromotions] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:8000/promotions?_embed=comments')
+
+    const params = {}
+    if(search) {
+      params.title_like = search
+    }
+
+    axios.get('http://localhost:8000/promotions?_embed=comments', { params })
       .then((response) => setPromotions(response.data))
-  }, [])
+  }, [search])
 
   return(
-    <div>
+    <div className="promotion-search">
+      <header className="promotion-search__header">
+        <h1>Promo Show</h1>
+        <Link to='/create'>Nova Promoção</Link>
+      </header>
+      <input 
+        type='search' 
+        className="promotion-search__input" 
+        placeholder="Buscar"
+        value={search}
+        onChange={ ev => setSearch(ev.target.value) }
+      />
       { promotions.map((promotion) => <PromotionCard promotion={promotion} /> )}
     </div>
   )
